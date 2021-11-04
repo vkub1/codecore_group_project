e04da31d00905dcccfe50cdbe3301430a6c333a6
Becca-Chae <dabin.chae55@gmail.com>
2021-11-04 13:33:28 -0700

    to solve issue with gemfile

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,11 +1,17 @@
 class EnrollmentsController < ApplicationController
 
   def index
-    
+    @enrollments = Enrollment.order(created_at: :desc)
+    @enrollments = Enrollment.find.params[:id]
+
+    if @enrollment.approved 
+
+    else
+    end
   end
 
   def create
-    @enrollment = Enrollment.new(params.require(:enrollment).permit(:use_id, :course_id))
+    @enrollment = Enrollment.new(params.require(:enrollment).permit(:user_id, :course_id))
     @enrollment.course = @course
     @enrollment.user = current_user
 
@@ -14,6 +20,15 @@ class EnrollmentsController < ApplicationController
       Notification.create
       redirect_to course_path(@course)
     end
+  end
+
+  def update
+  
+  end
+
+  def destroy
+    @enrollment = Enrollment.find.params[:id]
+
   end
 
 
