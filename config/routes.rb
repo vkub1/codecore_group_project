4461c3913bbea4e1bc9ce14df4cef9ab54ad24a8
Faiza Eba Batool <faizabatool1691@gmail.com>
2021-11-04 13:52:13 -0700

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get('/', {to: 'welcome#index', as: 'root'})
+
+  resources :users, only:[:new, :create]
+  resource :session, only:[:new, :create, :destroy]
+  get('/admin', {to: 'users#admin',as: 'admin'})
 end
