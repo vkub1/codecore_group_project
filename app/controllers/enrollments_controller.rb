78e1282888d7e399fdc992aa1672261a8c288e5a
Kevin Chen <chen.kcw@gmail.com>
2021-11-04 15:09:20 -0700

    Only show unread notifications

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -16,12 +16,10 @@ class EnrollmentsController < ApplicationController
   end
 
   def update
-    @enrollment
     Notification.create(message: "Your request to enroll in #{@enrollment.course_id.title} has been accepted", accepted: true, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
   end
 
   def destroy
-    @enrollment
     Notification.create(message: "Your request to enroll in #{@enrollment.course_id.title} has been declined", accepted: true, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
   end
 end
