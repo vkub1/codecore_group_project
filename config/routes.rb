b05eac2997ddc70d8c4ad29b0b134301e5a95d73
vkub1 <kubliyvlad@gmail.com>
2021-11-04 11:58:41 -0700

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   resources :facilities
   
   resource :session, only: [:new, :create, :destroy]
-  get('/admin', {to: "user#admin", as 'admin'})
+  get('/admin', {to: "users#admin", as 'admin'})
 
 end
