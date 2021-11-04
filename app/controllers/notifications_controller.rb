803cd9782798111d341b1560eb1b5416b84c11cb
Kevin Chen <chen.kcw@gmail.com>
2021-11-04 15:40:51 -0700

    Hide read notifications in index

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,4 +4,10 @@ class NotificationsController < ApplicationController
     @user = User.find params[:user_id]
     @notifications = @user.received_notifications
   end
+
+  def update
+    @notification = Notification.find params[:id]
+    @notification.update(read: true)
+    render :index
+  end
 end
