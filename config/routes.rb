b93506607c92a2fd963764a3ad22884c64fe1c92
vkub1 <kubliyvlad@gmail.com>
2021-11-04 13:09:27 -0700

    adding database.yml to gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-
   get('/', {to: "welcome#index", as: 'root'})
   resources :users, only: [:new, :create] do
     resources :notifications, only: [:index, :destroy]
