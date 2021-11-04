47a60f09556c75feedc334e8b9bdd334d69f9f30
Becca-Chae <dabin.chae55@gmail.com>
2021-11-04 12:01:03 -0700

    initial commit-add_approved_column_to_enrollments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_003752) do
+ActiveRecord::Schema.define(version: 2021_11_04_180315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_003752) do
     t.boolean "is_teacher", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "approved", default: false
     t.index ["course_id"], name: "index_enrollments_on_course_id"
     t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
