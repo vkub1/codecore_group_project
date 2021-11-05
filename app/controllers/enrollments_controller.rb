348d282bd1d6f29de8f5f5f34a593cb0053a87e1
Kevin Chen <chen.kcw@gmail.com>
2021-11-05 12:01:20 -0700

    Change notification enrollment msg

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -17,8 +17,8 @@ class EnrollmentsController < ApplicationController
     @teacher = Enrollment.where("is_teacher = ? AND course_id = ?", true, @course.id)[0]
     if @enrollment.valid?
       @enrollment.save
-      Notification.create(message: "#{@enrollment.user.first_name} requested to enroll in your course", accepted: false, sender_id: @enrollment.user.id, receiver_id: @teacher.user_id, is_request: true, request_type: "enrollment", enrollment_id: @enrollment.id)
-      flash[:alert] = "enrollment request has been sent!"
+      Notification.create(message: "#{@enrollment.user.first_name} has requested to enroll in your #{@course.title} course", accepted: false, sender_id: @enrollment.user.id, receiver_id: @teacher.user_id, is_request: true, request_type: "enrollment", enrollment_id: @enrollment.id)
+      flash[:notice] = "Enrollment request has been sent!"
       redirect_to course_path(@course)
     end
   end
@@ -49,7 +49,7 @@ class EnrollmentsController < ApplicationController
     else
       Notification.create(message: "#{@enrollment.user.first_name} has cancelled their enrollment for your #{@course.title} course", accepted: false, sender_id: @enrollment.user.id, receiver_id: @teacher.user_id, is_request: false)
       flash[:alert] = @enrollment.errors.full_messages
-      redirect_to courses_path, alert: "Your course is cancelled"
+      redirect_to courses_path, notice: "Your course is cancelled"
     end
   end
 
