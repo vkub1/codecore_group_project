324d0d0c791b8981230e9167c5a74d556f7ccc54
Harry Ji <jiharry@hotmail.com>
2021-11-04 22:57:28 -0700

    Harry submit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,8 +5,10 @@ class BookingsController < ApplicationController
     #before_action :authorize_user!
 
     def index
-        @bookings = Booking.order(created_at: :desc)
+        #@bookings = Booking.order(created_at: :desc)
          
+        @bookings = Booking.where(:course_id=>@course.id) 
+        #byebug
     end
 
     def new
@@ -14,11 +16,22 @@ class BookingsController < ApplicationController
     end
 
     def create
-
-        @course = Course.find params[:course_id]
-        @booking  = Bookings.new(params.require(:review).permit(:rate,:body))
-        @booking.product = Product.find params[:product_id]  
-        @booking.user = current_user
+  
+        #@booking  = Booking.new(params.require(:booking).permit(:facility_id, :comment , :start_time, :end_time))
+        @booking  = Booking.new(booking_params)
+        @booking.course_id = @course.id
+         
+        if @booking.save
+            
+            #AnswerMailer.new_answer(@answer).deliver_now
+            #Or try this   AnswerMailer.new_answer(@answer).deliver_later
+            #AnswerMailer.delay(run_at:  1.minutes.from_now).new_answer(@answer)
+            redirect_to course_bookings_path(@course.id), notice: 'Booking created!'
+        else
+           
+            render :new
+           
+        end
 
     end    
 
@@ -26,13 +39,30 @@ class BookingsController < ApplicationController
     end
 
     def destroy
+        #byebug
+        #@booking = Answer.find params[:id]
+       # if can?(:crud, @booking)
+            @booking.destroy
+            #byebug
+            redirect_to course_bookings_path(@course.id),  notice: 'Answer Deleted'
+      #  else
+       #     redirect_to root_path, alert: 'Not Authorized'
+      # end
     end    
 
     def edit
         
     end
     
-    def update       
+    def update      
+
+        if @booking.update(booking_params)
+            redirect_to course_bookings_path(@course.id), notice: 'Booking created!'
+        else
+            render :edit
+        end
+ 
+
     end
 
 
@@ -48,7 +78,9 @@ class BookingsController < ApplicationController
     end
     
     def booking_params
-        params.require(:booking).permit(:facility_id, :course_id, :start_date , :start_time , :end_date , :end_time, :comment )
+
+        #byebug
+        params.require(:booking).permit(:facility_id, :comment , :start_time, :end_time) 
     end
 
     def authorize_user!
