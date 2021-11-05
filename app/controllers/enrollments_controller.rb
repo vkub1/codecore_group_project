5aef6b0434ebeb9e95b6e9a44aba88905aed397d
Kevin Chen <chen.kcw@gmail.com>
2021-11-04 17:17:21 -0700

    Fix approve and deny links in notification index

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -11,26 +11,33 @@ class EnrollmentsController < ApplicationController
     @enrollment = Enrollment.new(user_id: params[:user_id], course_id: params[:course_id])
     @enrollment.course = @course
     @enrollment.user = current_user
-    @teacher = Enrollment.where("course_id == :course_id AND is_teacher == :bool", {course_id: params[:course_id], bool: true})
-    byebug
+    @teacher = Enrollment.where("course_id == :course_id AND is_teacher == :bool", {course_id: @course.id, bool: true})
     if @enrollment.valid?
       @enrollment.save
-      Notification.create(message: "#{@enrollment.user.first_name} requested to enroll in your course", accepted: false, sender_id: @enrollment.user.id, receiver_id: @teacher.id, is_request: true, request_type: "enrollment")
+      Notification.create(message: "#{@enrollment.user.first_name} requested to enroll in your course", accepted: false, sender_id: @enrollment.user.id, receiver_id: @teacher.id, is_request: true, request_type: "enrollment", enrollment_id: @enrollment.id)
       flash[:alert] = "enrollment request has been sent!"
       redirect_to course_path(@course)
     end
   end
 
   def update
-    n = Notification.find params[:id]
-    Notification.create(message: "Your request to enroll in #{@enrollment.course_id.title} has been accepted", accepted: true, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
-    Notification.update(message: "Your request to enroll in #{@enrollment.course_id.title} has been accepted", accepted: true, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
+    @enrollment = Enrollment.find params[:id]
+    @teacher = Enrollment.where("course_id == :course_id AND is_teacher == :bool", {course_id: @course.id, bool: true})
+    @notification = params[:nid]
+    @notification.update(read: true)
+    Notification.create(message: "Your request to enroll in #{@enrollment.course_id.title} has been approved", accepted: true, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
   end
 
   def destroy
-    
-    @enrollment = Enrollment.find.params[:id]
-    Notification.create(message: "Your request to enroll in #{@enrollment.course_id.title} has been declined", accepted: true, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
+    @enrollment = Enrollment.find params[:id]
+    @teacher = Enrollment.where("course_id == :course_id AND is_teacher == :bool", {course_id: @course.id, bool: true})
+    @notification = params[:nid]
+    @notification.update(read: true)
+    if (params[:notif])
+      Notification.create(message: "Your request to enroll in #{@enrollment.course_id.title} has been declined", accepted: false, sender_id: @teacher.id, receiver_id: @enrollment.user.id, is_request: false)
+    else
+      Notification.create(message: "#{@enrollment.user.first_name} has cancelled their enrollment for your #{@enrollment.course_id.title} course", accepted: false, sender_id: @enrollment.user.id, receiver_id: @teacher.id, is_request: false)
+    end
   end
 
 end
--- db/migrate/20211104233156_add_course_id_to_notifications.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseIdToNotifications < ActiveRecord::Migration[6.1]
-  def change
-    add_column :notifications, :course_id, :bigint
-  end
-end
